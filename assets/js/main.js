093b5f0f83c62fd7068740db88476ad78f56f446
beucismis <beucismis@tutamail.com>
2025-10-11 21:50:15 +0300

    Remove main.js

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll(".highlight > pre > code[data-lang]").forEach((code) => {
-    const language = code.getAttribute("data-lang");
-    const highlightDiv = code.closest(".highlight");
-    if (highlightDiv) {
-      highlightDiv.setAttribute("data-lang", language);
-    }
-  });
-});
