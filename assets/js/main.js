ccf2867c28a3a415006b49ca5a9383f0ca08b181
beucismis <beucismis@tutamail.com>
2025-10-12 10:10:07 +0300

    Update theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,14 @@
-// Bu fonksiyon, sayfa kaydırıldığında çalışacak
 function updateReadingProgressBar() {
-  // İlerleme çubuğu elementini seçelim
   const progressBar = document.querySelector(".reading-progress-bar");
 
-  // Eğer sayfada bu element yoksa, fonksiyonu sonlandır
   if (!progressBar) {
     return;
   }
 
-  // Kullanıcının ne kadar aşağı kaydırdığını bulalım
   const scrollTop = window.scrollY || document.documentElement.scrollTop;
-
-  // Sayfanın toplam kaydırılabilir yüksekliğini hesaplayalım
-  // document.documentElement.scrollHeight: Sayfanın toplam yüksekliği
-  // document.documentElement.clientHeight: Ekranın görünen yüksekliği
   const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-
-  // İlerleme yüzdesini hesaplayalım
   const scrollPercent = (scrollTop / scrollHeight) * 100;
-
-  // Çubuğun genişliğini (width) hesaplanan yüzdeye göre güncelleyelim
   progressBar.style.width = scrollPercent + "%";
 }
 
-// "scroll" olayını dinleyerek, her kaydırma hareketinde fonksiyonumuzu çalıştıralım
 window.addEventListener("scroll", updateReadingProgressBar);
